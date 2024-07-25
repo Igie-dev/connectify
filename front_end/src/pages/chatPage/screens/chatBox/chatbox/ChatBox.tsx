275cfed1b56679f0f22b0a190e512fd4ab515a3c
igie <baldesansoigieboysanchez@gmail.com>
2024-07-25 09:46:17 +0800

    add border

--- front_end/src/pages/chatPage/screens/chatBox/chatbox/ChatBox.tsx
+++ front_end/src/pages/chatPage/screens/chatBox/chatbox/ChatBox.tsx
@@ -2,7 +2,7 @@ import ChatInput from "./ChatInput";
 import MessageList from "./MessageList";
 export default function ChatBox() {
   return (
-    <section className="relative flex flex-col items-center flex-1 h-full pt-10">
+    <section className="relative flex flex-col items-center flex-1 h-full pt-10 border">
       <MessageList />
       <ChatInput />
     </section>
